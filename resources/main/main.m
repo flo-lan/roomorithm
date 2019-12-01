df1618dafd565430968ade5cb0d8fc2d8601d061
Flo <me@flo-lan.com>
2019-12-01 16:05:09 +0100

    first window detection approach

--- resources/main/main.m
+++ resources/main/main.m
@@ -1,5 +1,9 @@
 function main
  img = imread('img/Datei4.png');
- B = roomdetection(img);
- imshow(B)
+ % annotate windows
+ img2 = window_detection(img);
+ %imshowpair(img,img2,'montage')
+ imshow(img)
+ %B = roomdetection(img);
+ %imshow(B)
 end

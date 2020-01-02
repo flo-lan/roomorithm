eeb8d0bae80f44673fb0bcc248bf351d578241b0
00371366 <e0371366@student.tuwien.ac.at>
2020-01-02 15:40:20 +0100

    doorcounting

--- resources/main/main.m
+++ resources/main/main.m
@@ -1,11 +1,33 @@
 function main
- img = imread('img/Datei3.png', 'BackgroundColor',[1,1,1]);
+ img = imread('img/Datei4.png', 'BackgroundColor',[1,1,1]);
  % annotate windows
  img2 = remove_details(img);
  %img2 = imcomplement(img2);
  
  w_t = wall_thickness(img2)
- %imshowpair(img,img2,'montage');
+ imshowpair(img,img2,'montage');
  
+ sz = size(img2);
+ doors = 0;
+ countb = 0;
+ for i= 1:sz(1)
+    for j = 1:sz(2)
+    
+    if(img2(i,j) == 0)
+        countb = countb + 1;
+    end
+    
+    if(img2(i,j) == 1 && countb > 0)
+     if(countb > w_t*2 && countb < w_t*5)
+         doors = doors + 1;
+     end
+     countb = 0;
+    end
+    
+    end
+    countb = 0;
+ end
+ doors
+
  
 end

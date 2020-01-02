2820ed4f6bf8428f62f06753e77db9c061556181
marka <e11808580@student.tuwien.ac.at>
2020-01-02 17:25:54 +0100

    Some changes

--- resources/main/doordetection.m
+++ resources/main/doordetection.m
@@ -10,17 +10,17 @@ function output = doordetection(bin_img, w_t)
     C = sortrows(C)
     
     D = zeros(2);
-    
+
     if (abs(C(1,1)-C(2,1))<w_t*1.5)
-        D(end+1) = C(1,:);
+        D(end+1,:) = C(1,:);
     end
-    for i = 2:numel(C(1))-1
+    for i = 2:(size(C)-1)
         if ((abs(C(i,1)-C(i-1,1))<w_t*1.5) | (abs(C(i,1)-C(i+1,1))<w_t*1.5))
-           D(end+1) = C(i,:); 
+           D(end+1,:) = C(i,:); 
         end
     end
     if (abs(C(end,1)-C(end-1,1))<w_t*1.5)
-        D(end+1) = C(end,:);
+        D(end+1,:) = C(end,:);
     end
     D = D(3:end, :);
     

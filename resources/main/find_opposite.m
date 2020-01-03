be48d7cd115716b26b66dc5e069ed813bf4fe76b
00371366 <e0371366@student.tuwien.ac.at>
2020-01-03 18:25:50 +0100

    kopfschmerzen

--- resources/main/find_opposite.m
+++ resources/main/find_opposite.m
@@ -2,7 +2,7 @@ function possibleOpposites = find_opposite(centerpoint,normVec,centerpoints, w_t
 %FIND_OPPOSITE Author: Hoertner Filip
 %   finds opposite wall edge
 
-search = centerpoint+w_t*normVec;
+searchPoint = round(centerpoint+w_t*normVec);
 
 centerpoints = round(centerpoints);
 
@@ -10,14 +10,14 @@ F = zeros(2);
 
 for i=w_t:7*w_t
     
-    search = search+normVec;
+    searchPoint = round(searchPoint+normVec);
     
     for j=-20:20
         for k=-20:20
             
-            if (sum(centerpoints(:, 1) == (search(1)+j) & centerpoints(:, 2) == (search(2)+k))~=0)
+            if (sum(centerpoints(:, 1) == (searchPoint(1)+j) & centerpoints(:, 2) == (searchPoint(2)+k)) > 0)
                 
-                F(end+1,:) = [search(1)+j, search(2)+k];
+                F(end+1,:) = [searchPoint(1)+j, searchPoint(2)+k];
                 
             end
             

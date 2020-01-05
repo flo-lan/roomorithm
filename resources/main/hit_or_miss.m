c32fbc50c0e8b859b75b41dccb4b9ed4007629d6
Flo <me@flo-lan.com>
2020-01-05 19:48:07 +0100

    moved hit or miss to seperate file

---
+++ resources/main/hit_or_miss.m
@@ -0,0 +1,20 @@
+% A⊛B=(A⊖B1)∩(Ac⊖B2)
+% https://docs.opencv.org/master/db/d06/tutorial_hitOrMiss.html
+function [hitImg] = hit_or_miss(binImg, sElementFg, sElementBg)
+    % erode img with first structure element (A⊖B)
+    erodedImg = imerode(binImg,sElementFg);
+    
+    % img complement
+    imageComp = ~binImg;
+    
+    % erode complemented img Ac with second structure element.
+    erodedImg2 = imerode(imageComp,sElementBg);
+    
+    % intersection between erodedImg and erodedImg2
+    hitImg = zeros(size(binImg, 1), size(binImg,2));
+    for row = 1:size(erodedImg, 1)
+        for col = 1:size(erodedImg2, 2)
+            hitImg(row, col) = erodedImg(row,col) & erodedImg2(row,col);
+        end
+    end
+end
--- resources/main/windowdetection.m
+++ resources/main/windowdetection.m
@@ -13,7 +13,7 @@ function [window_count] = windowdetection(img)
                                1 0 0 0 0 0 0
                                1 0 0 1 1 1 1
                                1 0 0 1 1 1 1];
- hitImgLeftWindow = hitOrMiss(threshold_img, sElementLeftWindow, sElementLeftWindowNegative);
+ hitImgLeftWindow = hit_or_miss(threshold_img, sElementLeftWindow, sElementLeftWindowNegative);
  
  sElementRightWindow = [0 0 0 0 1 0 0
                         0 0 0 0 1 0 0
@@ -25,32 +25,11 @@ function [window_count] = windowdetection(img)
                                 0 0 0 0 0 0 1
                                 1 1 1 1 0 0 1
                                 1 1 1 1 0 0 1];
- hitImgRightWindow = hitOrMiss(threshold_img, sElementRightWindow, sElementRightWindowNegative);
+ hitImgRightWindow = hit_or_miss(threshold_img, sElementRightWindow, sElementRightWindowNegative);
  annotatedWindows = imoverlay(threshold_img, hitImgLeftWindow, 'red');
  annotatedWindows = imoverlay(annotatedWindows, hitImgRightWindow, 'green');
  %imshowpair(redHitImg,annotatedWindows,'montage');
  imshow(annotatedWindows);
  sum(hitImgRightWindow(:) == 1)
  window_count = sum(hitImgLeftWindow(:) == 1);
-end
-
-% A⊛B=(A⊖B1)∩(Ac⊖B2)
-% https://docs.opencv.org/master/db/d06/tutorial_hitOrMiss.html
-function [hitImg] = hitOrMiss(binImg, sElementFg, sElementBg)
-    % erode img with first structure element (A⊖B)
-    erodedImg = imerode(binImg,sElementFg);
-    
-    % img complement
-    imageComp = ~binImg;
-    
-    % erode complemented img Ac with second structure element.
-    erodedImg2 = imerode(imageComp,sElementBg);
-    
-    % intersection between erodedImg and erodedImg2
-    hitImg = zeros(size(binImg, 1), size(binImg,2));
-    for row = 1:size(erodedImg, 1)
-        for col = 1:size(erodedImg2, 2)
-            hitImg(row, col) = erodedImg(row,col) & erodedImg2(row,col);
-        end
-    end
 end

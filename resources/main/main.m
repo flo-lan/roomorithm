018f510030fa5c26b73d5d2b8e049be3c1a68f16
Flo <me@flo-lan.com>
2020-01-06 01:54:25 +0100

    loop through all pngs in image directory

--- resources/main/main.m
+++ resources/main/main.m
@@ -1,41 +1,45 @@
 function main
+ files = dir('./resources/main/img/*.png');
  file = [];
- windowCount = [];
- doorCount = [];
- roomCount = [];
- fileName = "img/Datei3.png";
- img = imread(fileName, 'BackgroundColor',[1,1,1]);
- file = [file, fileName];
- % annotate windows
- img2 = remove_details(img);
- %img2 = imcomplement(img2);
- 
- %w_t = wall_thickness(img2)
- 
- %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
- w_tc = wall_thiccness(img2)
- %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
-  
- %imshowpair(img,img2,'montage');
- windowCountResult = windowdetection(img, w_tc);
- windowCount = [windowCount, windowCountResult];
- 
- [doorCountResult,img3] = doordetection(img2, w_tc);
- doorCount = [doorCount, doorCountResult];
- %{
- doorCount
- figure;
- imshow(img3);
- %}
- %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
- 
- %[doorCount,img_3] = door_detection(img2, w_tc);
+ windowCount = zeros(size(files));
+ doorCount = zeros(size(files));
+ roomCount = zeros(size(files));
+ for k=1:length(files)
+     fileName = files(k).name;
 
- figure;
- imshow(img3);
- 
- rooms = roomdetection(img3);
- roomCount = [roomCount, numel(rooms)/2];
- %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
+     img = imread(fileName, 'BackgroundColor',[1,1,1]);
+     file = [file; fileName];
+     % annotate windows
+     img2 = remove_details(img);
+     %img2 = imcomplement(img2);
+
+     %w_t = wall_thickness(img2)
+
+     %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
+     w_tc = wall_thiccness(img2);
+     %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
+
+     %imshowpair(img,img2,'montage');
+     windowCountResult = windowdetection(img, w_tc);
+     windowCount(k) = windowCountResult;
+
+     [doorCountResult,img3] = doordetection(img2, w_tc);
+     doorCount(k) = doorCountResult;
+     %{
+     doorCount
+     figure;
+     imshow(img3);
+     %}
+     %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
+
+     %[doorCount,img_3] = door_detection(img2, w_tc);
+
+     figure;
+     imshow(img3);
+
+     rooms = roomdetection(img3);
+     roomCount(k) = numel(rooms)/2;
+     %%%%%%%%%%%%%%%% MARK %%%%%%%%%%%%%%%
+ end
  jsonencode(table(file, windowCount, doorCount, roomCount))
 end
